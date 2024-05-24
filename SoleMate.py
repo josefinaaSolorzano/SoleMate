90e8516e2964817c10d5568fea792b96e2ebaeea
josefinaaSolorzano <167581166+josefinaaSolorzano@users.noreply.github.com>
2024-05-24 18:54:37 -0300

    Add files via upload

--- SoleMate.py
+++ SoleMate.py
@@ -1,32 +1,25 @@
-from keras.models import load_model
-from PIL import Image, ImageOps
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
-import streamlit as st
-import os
+import streamlit as st 
+#from openai import OpenAI
 
-from tensorflow.keras.layers import DepthwiseConv2D
-import tensorflow as tf
 
-# Custom DepthwiseConv2D that ignores the 'groups' argument
-class CustomDepthwiseConv2D(DepthwiseConv2D):
-    def __init__(self, *args, **kwargs):
-        kwargs.pop('groups', None)  # Remove the groups argument
-        super().__init__(*args, **kwargs)
 
-# Custom object scope
-custom_objects = {"DepthwiseConv2D": CustomDepthwiseConv2D}
+def classify_fruit(img):
 
-# Load the model with the custom object scope
-model = load_model("keras_model.h5", custom_objects=custom_objects, compile=False)
-
-# Load the labels
-class_names = open("labels.txt", "r").readlines()
-
-def classify_waste(img):
     # Disable scientific notation for clarity
     np.set_printoptions(suppress=True)
 
+    # Load the model
+    model = load_model("keras_model.h5", compile=False)
+
+    # Load the labels
+    class_names = open("labels.txt", "r").readlines()
+
     # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
     # Replace this with the path to your image
@@ -48,10 +41,47 @@ def classify_waste(img):
     # Predicts the model
     prediction = model.predict(data)
     index = np.argmax(prediction)
-    class_name = class_names[index].strip()
+    class_name = class_names[index]
     confidence_score = prediction[0][index]
 
+    # Print prediction and confidence score
+    # print("Class:", class_name[2:], end="")
+    # print("Confidence Score:", confidence_score)
+
     return class_name, confidence_score
+
+
+
+#CATA def classify_waste(img):
+    # Disable scientific notation for clarity
+    # np.set_printoptions(suppress=True)
+
+    # Create the array of the right shape to feed into the keras model
+    # data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    # Replace this with the path to your image
+    # image = img.convert("RGB")
+
+    # resizing the image to be at least 224x224 and then cropping from the center
+   #size = (224, 224)
+   # image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    # turn the image into a numpy array
+#    image_array = np.asarray(image)
+
+    # Normalize the image
+    #normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # Load the image into the array
+    #data[0] = normalized_image_array
+
+    # Predicts the model
+    #prediction = model.predict(data)
+    #index = np.argmax(prediction)
+    #class_name = class_names[index].strip()
+    #confidence_score = prediction[0][index]
+
+    #return class_name, confidence_score
 
 st.set_page_config(layout='wide')
 
@@ -76,44 +106,17 @@ if input_img is not None:
         with col2:
             st.info("Your Result")
             image_file = Image.open(input_img)
-            label, confidence_score = classify_waste(image_file)
+            label, confidence_score = classify_fruit(image_file)
             if label == "0 Air Forces":
-                st.success("The image is classified as Air Forces.")                
+                st.success("Tu par perfecto es Air Forces.")                
             elif label == "1 Air Jordans":
-                st.success("The image is classified as Air Jordans.")
+                st.success("Tu par perfecto es Air Jordans.")
             elif label == "2 Air Maxes":
-                st.success("The image is classified as Air Maxes.")
+                st.success("Tu par perfecto es Air Maxes.")
             elif label == "3 Cleats":
-                st.success("The image is classified as Cleats.")
+                st.success("Tu par perfecto es Cleats.")
             elif label == "4 Dunks":
-                st.success("The image is classified as Dunks.")
+                st.success("Tu par perfecto es Dunks.")
             else:
-                st.error("The image is not classified as any relevant class.")
+                st.error("No encontramos ningún match para vos 😢 Cargá otra foto para que encontremos tu par ideal! .")
 
-st.title('_Ya tenes tu par?_')
-st.subheader('_Hacé click en los modelos para encontrar tu SoleMate_', divider='red')
-
-
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-   st.image("airfORces.jpg")
-   if st.button("Air Forces", type="primary"):
-    st.write('<a href="https://www.nike.com.ar/air%201%20force?_q=air%201%20force&map=ft" target="_blank">Haz clic aquí para ir a la página</a>', unsafe_allow_html=True)
-   
-
-with col2:
-   st.image("airMaxestest.jpg")
-   if st.button("Air Maxes", type="primary"):
-    st.write('<a href="https://www.nike.com.ar/air%20max?_q=air%20max&map=ft" target="_blank">Haz clic aquí para ir a la página</a>', unsafe_allow_html=True)
-
-with col3:
-   st.image("jordan1.jpg")
-   if st.button("Jordans", type="primary"):
-    st.write('<a href="https://www.nike.com.ar/jordan?_q=jordan&map=ft&page=2" target="_blank">Haz clic aquí para ir a la página</a>', unsafe_allow_html=True)
-
-with col4:
-   st.image("dunkstest.jpg")
-   if st.button("Dunks", type="primary"):
-    st.write('<a href="https://www.nike.com.ar/dunks?_q=dunks&map=ft&page=2" target="_blank">Haz clic aquí para ir a la página</a>', unsafe_allow_html=True)
-   
